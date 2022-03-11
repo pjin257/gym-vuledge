664030df1932bf1b4eb16da214403d58821b03b6
Jinyoung Pung <pjin257@gmail.com>
2022-03-11 13:30:06 -0800

    Evaluation environment
    An environment for evaluation is added. In this environment, statistic information is saved at the end of each episode. The data can be used to observe the behavior of the agent during each episode

--- gym_vuledge/__init__.py
+++ gym_vuledge/__init__.py
@@ -4,6 +4,11 @@ from gym.envs.registration import register
 logger = logging.getLogger(__name__)
 
 register(
-    id='vuledge-v0',
+    id='vuledge-v1',
     entry_point='gym_vuledge.envs:VulEdgeEnv',
+)
+
+register(
+    id='eval-v1',
+    entry_point='gym_vuledge.envs:EvalEnv',
 )
--- gym_vuledge/envs/__init__.py
+++ gym_vuledge/envs/__init__.py
@@ -1,4 +1,6 @@
 """VulEdge Gym Enviornments."""
 
 from gym_vuledge.envs.vuledge_env import VulEdgeEnv
+from gym_vuledge.envs.eval_env import EvalEnv
 from gym_vuledge.envs.roadnet import GV, Traffic_Gen, Vehicle, Moving_Process, Reroute_Process, Edge_Attack, ROADNET
+from gym_vuledge.envs.evalnet import GV, Traffic_Gen, Vehicle, Moving_Process, Reroute_Process, Edge_Attack, ROADNET
