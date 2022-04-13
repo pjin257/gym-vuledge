1e29af8882bb1dca6ec8c983a42649751f6042b1
Jinyoung Pung <pjin257@gmail.com>
2022-04-13 13:14:34 -0700

    err correction

--- gym_vuledge/envs/__init__.py
+++ gym_vuledge/envs/__init__.py
@@ -2,5 +2,7 @@
 
 from gym_vuledge.envs.vuledge_env import VulEdgeEnv
 from gym_vuledge.envs.eval_env import EvalEnv
+from gym_vuledge.envs.reverse_env import ReverseEnv
 from gym_vuledge.envs.roadnet import GV, Traffic_Gen, Vehicle, Moving_Process, Reroute_Process, Edge_Attack, ROADNET
 from gym_vuledge.envs.evalnet import GV, Traffic_Gen, Vehicle, Moving_Process, Reroute_Process, Edge_Attack, ROADNET
+from gym_vuledge.envs.reversenet import GV, Traffic_Gen, Vehicle, Moving_Process, Reroute_Process, Edge_Attack, ROADNET
