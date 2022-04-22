4e8e607525336d4d33f326b65c566b9908039910
Jinyoung Pung <pjin257@gmail.com>
2022-04-22 13:27:50 -0700

    error correction
    corrected entry point name

--- gym_vuledge/__init__.py
+++ gym_vuledge/__init__.py
@@ -10,17 +10,17 @@ register(
 
 register(
     id='vuledge2d-v1',
-    entry_point='gym_vuledge.envs:VulEdgeEnv',
+    entry_point='gym_vuledge.envs:VulEdge2dEnv',
 )
 
 register(
     id='vuledge3d-v1',
-    entry_point='gym_vuledge.envs:VulEdgeEnv',
+    entry_point='gym_vuledge.envs:VulEdge3dEnv',
 )
 
 register(
     id='vuledge4d-v1',
-    entry_point='gym_vuledge.envs:VulEdgeEnv',
+    entry_point='gym_vuledge.envs:VulEdge4dEnv',
 )
 
 register(
